1618cc2b58435edee83960a86202e4d9397b2149
Asta Remeikiene <65591369+AstaRem@users.noreply.github.com>
2023-05-07 09:17:03 +0100

    added questions and generateMarkdown text

--- index.js
+++ index.js
@@ -5,15 +5,53 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+      },
+    {
+    type: 'input',
+    name: 'description',
+    message: 'Describe your project:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How to install your app/project?',
+      },
+    {
+    type: 'input',
+    name: 'usage',
+    message: 'How to use it?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Your email: ',
+      },
+    {
+    type: 'input',
+    name: 'tests',
+    message: 'How to run the test?',
+    },
+    {
+        type: 'input',
+        name: 'github_URL',
+        message: 'Your GitHub url:',
+      }
+     
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+// function writeToFile(fileName, data) {
+// }
 
 // function to initialize program
 function init() {
+    inquirer.prompt(questions).then(function(answers){
+        console.log(answers)
+    });
 
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,52 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Description
+${description}
+
+## Table of content
+
+- [Title](#title)
+- [Description](#description)
+- [Table of content](#table-of-content)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+  
+## Installation
+
+${installation}
+
+## Usage
+
+${usage}
+
+## License  
+
+GNU General Public License, version 3 (GPLv3)
+Apache License 2.0
+Berkeley Software Distribution (BSD)
+MIT License
+
+
+## Contributing
+
+If you would like to contribute to this project, please contact me via email: ${email}.
+I will provide you with contribution guidelines.
+
+## Tests
+
+${tests}
+
+## Questions
+To view my GitHub profile, please click [this link](${github_URL}).
+
+If you have any questions, please contact me: ${email}.
+
+
 `;
 }
 
